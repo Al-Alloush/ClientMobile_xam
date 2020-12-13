35e27fd94373d4d6372dace291bf7d56fbd03784
Ahmad AlAlloush <ahmad.alloush@gmail.com>
2020-12-13 18:10:12 +0100

    Add Loginpage to buttom bar

---
+++ MobileV1/ViewModels/AccountPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileV1.ViewModels
+{
+    class AccountPageViewModel
+    {
+    }
+}
---
+++ MobileV1/Views/AccountPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MobileV1.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileV1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AccountPage : ContentPage
+    {
+        public AccountPage()
+        {
+            BindingContext = new AccountPageViewModel();
+            InitializeComponent();
+        }
+    }
+}
